753268d1133dc80b284a46cdd1708804c9f888d3
Andrew Kofink <ajkofink@gmail.com>
2017-07-26 05:43:20 -0400

    Add a katello_lifecycle_environment module (#21)

--- module_utils/ansible_nailgun_cement.py
+++ module_utils/ansible_nailgun_cement.py
@@ -6,7 +6,13 @@ import re
 import yaml
 
 from nailgun.config import ServerConfig
-import nailgun.entities
+from nailgun.entities import (
+    LifecycleEnvironment,
+    Location,
+    Organization,
+    Ping,
+    TemplateKind,
+)
 
 
 # Mix compare functionality into some entities as needed
@@ -19,15 +25,15 @@ class EntityCompareMixin:
         return self.id != other.id
 
 
-class TemplateKind(EntityCompareMixin, nailgun.entities.TemplateKind):
+class TemplateKind(EntityCompareMixin, TemplateKind):
     pass
 
 
-class Organization(EntityCompareMixin, nailgun.entities.Organization):
+class Organization(EntityCompareMixin, Organization):
     pass
 
 
-class Location(EntityCompareMixin, nailgun.entities.Location):
+class Location(EntityCompareMixin, Location):
     pass
 
 
@@ -38,6 +44,30 @@ def create_server(server_url, auth, verify_ssl):
         auth=auth,
         verify=verify_ssl,
     ).save()
+
+
+# Prerequisite: create_server
+def ping_server(module):
+    try:
+        return Ping().search_json()
+    except Exception as e:
+        module.fail_json(msg="Failed to connect to Foreman server: %s " % e)
+
+
+def update_fields(new, old, fields):
+    needs_update = False
+    for field in fields:
+        if hasattr(new, field) and hasattr(old, field):
+            new_attr = getattr(new, field)
+            old_attr = getattr(old, field)
+            id_attrs = hasattr(new_attr, 'id') and hasattr(old_attr, 'id')
+            if old_attr is None or (id_attrs and new_attr.id != old_attr.id) or (not id_attrs and new_attr != old_attr):
+                setattr(old, field, new_attr)
+                needs_update = True
+        elif hasattr(old, field) and getattr(old, field) is not None and not hasattr(new, field):
+            setattr(old, field, None)
+            needs_update = True
+    return needs_update, old
 
 
 # Common functionality to manipulate entities
@@ -70,7 +100,7 @@ def find_entities(entity_class, name_list, module):
     for element in name_list:
         try:
             return_list.append(find_entity(entity_class, name=element)[0])
-        except Exception as e:
+        except Exception:
             module.fail_json(
                 msg='Could not find the {0} {1}'.format(
                     entity_class.__name__, element))
@@ -138,3 +168,30 @@ def parse_template_from_file(file_name, module):
     except Exception as e:
         module.fail_json(msg='Error while reading template file: ' + str(e))
     return template_dict
+
+
+def find_organization(module, name, failsafe=False):
+    org = Organization(name=name)
+    response = org.search(set(), {'search': 'name="{}"'.format(name)})
+    return handle_find_response(module, response, message="No organization found for %s" % name, failsafe=failsafe)
+
+
+def find_lifecycle_environment(module, name, organization, failsafe=False):
+    response = LifecycleEnvironment(name=name, organization=organization).search()
+    return handle_find_response(module, response, message="No lifecycle environment found for %s" % name, failsafe=failsafe)
+
+
+def handle_find_response(module, response, message=None, failsafe=False):
+    message = "Find failed for entity: %s" % response if message is None else message
+    if len(response) == 1:
+        return response[0]
+    elif failsafe:
+        return None
+    else:
+        module.fail_json(msg=message)
+
+
+def current_subscription_manifest(module, organization):
+    org_json = organization.read_json()
+    if 'owner_details' in org_json and 'upstreamConsumer' in org_json['owner_details']:
+        return org_json['owner_details']['upstreamConsumer']
