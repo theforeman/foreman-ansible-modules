1eaf39b9320509b08ef9b5716f3cddb048828f91
Matthias Dellweg <dellweg@atix.de>
2018-01-19 13:28:43 +0100

    Add 'body' to the vcr match criteria
    Actually, we need to write our own matcher, because the key="value"
    pairs in the search parameter are not saved in the same order in
    different versions of python.

--- test/vcr_python_wrapper.py
+++ test/vcr_python_wrapper.py
@@ -4,6 +4,25 @@ import os
 import sys
 import vcr
 import json
+import re
+
+
+# We need our own json level2 matcher, because, python2 and python3 do not save
+# dictionaries in the same order
+def body_json_l2_matcher(r1, r2):
+    if r1.headers.get('content-type') == 'application/json' and r2.headers.get('content-type') == 'application/json':
+        if r1.body is None or r2.body is None:
+            return r1.body == r2.body
+        body1 = json.loads(r1.body)
+        body2 = json.loads(r2.body)
+        if 'search' in body1:
+            body1['search'] = ','.join(sorted(re.findall(r'([^=,]*="(?:[^"]|\\")*")', body1['search'])))
+        if 'search' in body2:
+            body2['search'] = ','.join(sorted(re.findall(r'([^=,]*="(?:[^"]|\\")*")', body2['search'])))
+        return body1 == body2
+    else:
+        return r1.body == r2.body
+
 
 VCR_PARAMS_FILE = os.environ['FAM_TEST_VCR_PARAMS_FILE']
 
@@ -21,11 +40,13 @@ with open(VCR_PARAMS_FILE, 'w') as params_file:
     json.dump(test_params, params_file)
 
 # Call the original python script with vcr-cassette in place
-with vcr.use_cassette(cassette_file,
-                      record_mode=test_params['record_mode'],
-                      match_on=['method', 'scheme', 'port', 'path', 'query'],
-                      filter_headers=['Authorization'],
-                      ):
+fam_vcr = vcr.VCR()
+fam_vcr.register_matcher('body_json_l2', body_json_l2_matcher)
+with fam_vcr.use_cassette(cassette_file,
+                          record_mode=test_params['record_mode'],
+                          match_on=['method', 'scheme', 'port', 'path', 'query', 'body_json_l2'],
+                          filter_headers=['Authorization'],
+                          ):
     with open(sys.argv[0]) as f:
         code = compile(f.read(), sys.argv[0], 'exec')
         exec(code)
