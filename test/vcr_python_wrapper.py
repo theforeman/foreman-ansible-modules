53fd05f378239b292e502bc3a92720fafe8b0c34
Matthias Dellweg <dellweg@atix.de>
2018-01-22 15:53:49 +0100

    Re-add 'body' to the vcr match criteria (#96)
    * Add 'body' to the vcr match criteria
    
    Actually, we need to write our own matcher, because the key="value"
    pairs in the search parameter are not saved in the same order in
    different versions of python.
    
    * Add python36 to the test matrix
    
    * Decode bodies from utf8 before parsing into json
    
    (cherry picked from commit 83c3162237df9c341472978d2f3d793d8ca52dd6)

--- test/vcr_python_wrapper.py
+++ test/vcr_python_wrapper.py
@@ -7,6 +7,23 @@ import json
 import re
 
 
+# We need our own json level2 matcher, because, python2 and python3 do not save
+# dictionaries in the same order
+def body_json_l2_matcher(r1, r2):
+    if r1.headers.get('content-type') == 'application/json' and r2.headers.get('content-type') == 'application/json':
+        if r1.body is None or r2.body is None:
+            return r1.body == r2.body
+        body1 = json.loads(r1.body.decode('utf8'))
+        body2 = json.loads(r2.body.decode('utf8'))
+        if 'search' in body1:
+            body1['search'] = ','.join(sorted(re.findall(r'([^=,]*="(?:[^"]|\\")*")', body1['search'])))
+        if 'search' in body2:
+            body2['search'] = ','.join(sorted(re.findall(r'([^=,]*="(?:[^"]|\\")*")', body2['search'])))
+        return body1 == body2
+    else:
+        return r1.body == r2.body
+
+
 VCR_PARAMS_FILE = os.environ.get('FAM_TEST_VCR_PARAMS_FILE')
 
 # Remove the name of the wrapper from argv
@@ -31,9 +48,10 @@ else:
 
     # Call the original python script with vcr-cassette in place
     fam_vcr = vcr.VCR()
+    fam_vcr.register_matcher('body_json_l2', body_json_l2_matcher)
     with fam_vcr.use_cassette(cassette_file,
                               record_mode=test_params['record_mode'],
-                              match_on=['method', 'scheme', 'port', 'path', 'query'],
+                              match_on=['method', 'scheme', 'port', 'path', 'query', 'body_json_l2'],
                               filter_headers=['Authorization'],
                               ):
         with open(sys.argv[0]) as f:
