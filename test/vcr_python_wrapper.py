0ca16a8d53b05bd871107dec8a27b3c861880611
Matthias Dellweg <dellweg@atix.de>
2018-01-22 14:06:02 +0100

    Decode bodies from utf8 before parsing into json

--- test/vcr_python_wrapper.py
+++ test/vcr_python_wrapper.py
@@ -13,8 +13,8 @@ def body_json_l2_matcher(r1, r2):
     if r1.headers.get('content-type') == 'application/json' and r2.headers.get('content-type') == 'application/json':
         if r1.body is None or r2.body is None:
             return r1.body == r2.body
-        body1 = json.loads(r1.body)
-        body2 = json.loads(r2.body)
+        body1 = json.loads(r1.body.decode('utf8'))
+        body2 = json.loads(r2.body.decode('utf8'))
         if 'search' in body1:
             body1['search'] = ','.join(sorted(re.findall(r'([^=,]*="(?:[^"]|\\")*")', body1['search'])))
         if 'search' in body2:
