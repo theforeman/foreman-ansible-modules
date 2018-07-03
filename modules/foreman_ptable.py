9888077999802bf467a08a1b88e2b9307a5209ed
Bernhard Suttner <suttner@atix.de>
2018-07-03 13:01:31 +0200

    Update version dependency of nailgun

--- modules/foreman_ptable.py
+++ modules/foreman_ptable.py
@@ -34,7 +34,7 @@ author:
 - "Bernhard Hopfenmueller (@Fobhep) ATIX AG"
 - "Matthias Dellweg (@mdellweg) ATIX AG"
 requirements:
-    - nailgun >= 0.29.0
+    - nailgun > 0.31.0
 options:
     server_url:
         description:
