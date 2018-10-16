7436a7d13fade9f6a0c56f5b3a22104f9095298c
Evgeni Golov <evgeni@golov.de>
2018-10-16 09:09:04 +0200

    drop simplejson
    nobody wants to support Python 2.4 :)

--- test/ansible_fail.py
+++ test/ansible_fail.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 import sys
-import simplejson
+import json
 
 message = """WARNING: You are trying to run unfit playbooks in automated testing.
 Have a look at: https://github.com/theforeman/foreman-ansible-modules/blob/master/test/README.md
 """
-sys.stdout.write(simplejson.dumps({'failed': True, 'stdout': message}))
+sys.stdout.write(json.dumps({'failed': True, 'stdout': message}))
 sys.stdout.write('\n')
