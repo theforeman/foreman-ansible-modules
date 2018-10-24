cd744e13689e94a0972098fcb6b3682075b14f08
Bernhard Suttner <sbernhard@users.noreply.github.com>
2018-10-24 15:12:29 +0200

    Adding locked state to ptable (#135)
    * Adding locked state to ptable
    
    Requires:
    - https://github.com/theforeman/foreman/pull/5741
    - https://github.com/SatelliteQE/nailgun/pull/503
    
    * Remove default value for locked state
    
    Remove the default value for the locked state otherwise the locked state
    will be set to "false" in case the "locked" option is not given. Set the
    locked state to true/false if you want to change the locked state. In
    case of a new template, the locked state is set to false automatically
    as this is the default on the foreman side.
    
    * Update version dependency of nailgun
    
    * Adding tests for locked state

--- modules/foreman_ptable.py
+++ modules/foreman_ptable.py
@@ -34,7 +34,7 @@ author:
 - "Bernhard Hopfenmueller (@Fobhep) ATIX AG"
 - "Matthias Dellweg (@mdellweg) ATIX AG"
 requirements:
-    - nailgun >= 0.29.0
+    - nailgun > 0.31.0
 options:
     server_url:
         description:
@@ -73,6 +73,11 @@ options:
         - The locations the template should be assigend to
         required: false
         type: list
+    locked:
+        description:
+        - Determines whether the template shall be locked
+        required: false
+        type: bool
     name:
         description:
         - |
@@ -262,6 +267,7 @@ name_map = {
     'template': 'layout',  # the parse_template_* methods stores the "layout" in "template"
     'layout': 'layout',
     'locations': 'location',
+    'locked': 'locked',
     'name': 'name',
     'organizations': 'organization',
     'oses': 'os_family',  # the foreman community templates are using oses instead of os_family (which is wrong?)
@@ -269,7 +275,6 @@ name_map = {
 }
 # Missing parameters:
 # snippet
-# locked
 # audit_comment
 # default
 
@@ -285,7 +290,7 @@ def main():
             layout=dict(),
             file_name=dict(type='path'),
             locations=dict(type='list'),
-            # locked=dict(type='bool', default=False),
+            locked=dict(type='bool'),
             name=dict(),
             organizations=dict(type='list'),
             os_family=dict(choices=list(_OPERATING_SYSTEMS)),
