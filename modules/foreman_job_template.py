e1d7536f52f582ceb5072ec215be3099bb4d8812
Evgeni Golov <evgeni@golov.de>
2018-12-12 11:05:21 +0100

    small fixes to with_filetree examples in job_template and ptable

--- modules/foreman_job_template.py
+++ modules/foreman_job_template.py
@@ -260,7 +260,7 @@ EXAMPLES = '''
     password:  "changeme"
     state: present
     layout: '{{ lookup("file", item.src) }}'
-  with_filetree: '{{ /path/to/Job/Templates/templates" }}'
+  with_filetree: '/path/to/job/templates'
   when: item.state == 'file'
 
 
