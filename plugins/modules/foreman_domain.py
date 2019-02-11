c8e3487051ec9ed573bf60c51370d249f2c32437
Evgeni Golov <evgeni@golov.de>
2019-02-11 07:31:40 +0100

    move common documentation to a fragment
    As of Ansible 2.8, we can ship custom documentation fragments together
    with our modules [1], so let's do this.
    
    (Documentation fragments were supported for upstream Ansible modules
    since at least 2.3, but you couldn't add more paths to search for them
    until 2.8)
    
    [1] https://docs.ansible.com/ansible/devel/dev_guide/developing_modules_documenting.html#documentation-fragments

--- plugins/modules/foreman_domain.py
+++ plugins/modules/foreman_domain.py
@@ -47,24 +47,11 @@ options:
     required: false
     default: None
     type: list
-  server_url:
-    description: foreman url
-    required: true
-  username:
-    description: foreman username
-    required: true
-  password:
-    description: foreman user password
-    required: true
-  validate_certs:
-    aliases: [ verify_ssl ]
-    description: verify ssl connection when communicating with foreman
-    default: true
-    type: bool
   state:
     description: domain presence
     default: present
     choices: ["present", "absent"]
+extends_documentation_fragment: foreman
 '''
 
 EXAMPLES = '''
