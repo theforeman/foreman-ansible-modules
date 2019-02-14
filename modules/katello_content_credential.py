104f18de38432e8fe7c70117dc919fcaa505a172
Evgeni Golov <evgeni@golov.de>
2019-02-14 14:30:48 +0100

    refactor katello_content_credential.py

--- modules/katello_content_credential.py
+++ modules/katello_content_credential.py
@@ -101,12 +101,10 @@ try:
         naildown_entity_state,
         sanitize_entity_dict,
     )
-    has_import_error = False
-except ImportError as e:
-    has_import_error = True
-    import_error_msg = str(e)
+except ImportError:
+    pass
 
-from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.foreman_helper import ForemanEntityAnsibleModule
 
 
 # This is the only true source for names (and conversions thereof)
@@ -119,34 +117,20 @@ name_map = {
 
 
 def main():
-    module = AnsibleModule(
+    module = ForemanEntityAnsibleModule(
         argument_spec=dict(
-            server_url=dict(required=True),
-            username=dict(required=True, no_log=True),
-            password=dict(required=True, no_log=True),
-            verify_ssl=dict(type='bool', default=True),
             name=dict(required=True),
             organization=dict(required=True),
             content_type=dict(required=True, choices=['gpg_key', 'cert']),
             content=dict(required=True),
-            state=dict(default='present', choices=['present', 'absent']),
         ),
         supports_check_mode=True,
     )
 
-    if has_import_error:
-        module.fail_json(msg=import_error_msg)
-
-    entity_dict = dict(
-        [(k, v) for (k, v) in module.params.items() if v is not None])
-
-    server_url = entity_dict.pop('server_url')
-    username = entity_dict.pop('username')
-    password = entity_dict.pop('password')
-    verify_ssl = entity_dict.pop('verify_ssl')
-    state = entity_dict.pop('state')
+    (server_params, entity_dict, state) = module.parse_params()
 
     try:
+        (server_url, username, password, verify_ssl) = server_params
         create_server(server_url, (username, password), verify_ssl)
     except Exception as e:
         module.fail_json(msg="Failed to connect to Foreman server: %s " % e)
