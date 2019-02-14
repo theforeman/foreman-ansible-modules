bbcc8c67494298ebba4c4ec1511d34b9b08f6d91
Evgeni Golov <evgeni@golov.de>
2019-02-14 14:30:48 +0100

    refactor katello_content_view.py

--- modules/katello_content_view.py
+++ modules/katello_content_view.py
@@ -132,13 +132,10 @@ try:
         naildown_entity,
         sanitize_entity_dict,
     )
+except ImportError:
+    pass
 
-    has_import_error = False
-except ImportError as e:
-    has_import_error = True
-    import_error_msg = str(e)
-
-from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.foreman_helper import ForemanEntityAnsibleModule
 
 name_map = {
     'name': 'name',
@@ -157,12 +154,8 @@ cvc_map = {
 
 
 def main():
-    module = AnsibleModule(
+    module = ForemanEntityAnsibleModule(
         argument_spec=dict(
-            server_url=dict(required=True),
-            username=dict(required=True, no_log=True),
-            password=dict(required=True, no_log=True),
-            verify_ssl=dict(type='bool', default=True),
             name=dict(required=True),
             organization=dict(required=True),
             composite=dict(type='bool', default=False),
@@ -175,19 +168,10 @@ def main():
         mutually_exclusive=[['repositories', 'components']],
     )
 
-    if has_import_error:
-        module.fail_json(msg=import_error_msg)
-
-    entity_dict = dict(
-        [(k, v) for (k, v) in module.params.items() if v is not None])
-
-    server_url = entity_dict.pop('server_url')
-    username = entity_dict.pop('username')
-    password = entity_dict.pop('password')
-    verify_ssl = entity_dict.pop('verify_ssl')
-    state = entity_dict.pop('state')
+    (server_params, entity_dict, state) = module.parse_params()
 
     try:
+        (server_url, username, password, verify_ssl) = server_params
         create_server(server_url, (username, password), verify_ssl)
     except Exception as e:
         module.fail_json(msg="Failed to connect to Foreman server: %s " % e)
