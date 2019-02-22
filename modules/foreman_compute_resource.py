84840acd423cf5b0226550acda21046936ce93a2
Baptiste Agasse <baptiste.agasse@gmail.com>
2019-02-22 14:26:27 +0100

    Refactor compute_resource, improve tests (#231)

--- modules/foreman_compute_resource.py
+++ modules/foreman_compute_resource.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # (c) Philipp Joos 2017
+# (c) Baptiste Agasse 2019
 #
 # This file is part of Ansible
 #
@@ -23,23 +24,33 @@ module: foreman_compute_resource
 short_description: Manage Foreman Compute resources using Foreman API
 description:
   - Create and delete Foreman Compute Resources using Foreman API
+author:
+  - "Philipp Joos (@philippj)"
+  - "Baptiste Agasse (@bagasse)"
 requirements:
-  - "nailgun >= 0.28.0"
+  - "nailgun >= 0.32.0"
   - "ansible >= 2.3"
 options:
   name:
     description: compute resource name
     required: true
+  updated_name:
+    description: new compute resource name
+    required: false
   description:
     description: compute resource description
     required: false
   provider:
-    description: provider
+    description: Compute resource provider. Required if I(state=present).
     required: false
     default: None
     choices: ["vmware", "libvirt", "ovirt"]
   provider_auth:
-    description: provider authentication
+    description: Deprecated, use I(provider_params) instead. Provider authentication
+    required: false
+    default: None
+  provider_params:
+    description: Parameter specific to compute resource provider
     required: false
     default: None
   locations:
@@ -72,24 +83,43 @@ options:
     default: present
     choices: ["present", "absent", "present_with_defaults"]
 version_added: "2.0"
-author: "Philipp Joos (@philippj)"
 '''
 
 EXAMPLES = '''
 - name: vmware compute resource
   foreman_compute_resource:
     name: example_compute_resource
-    datacenter: ax01
     locations:
       - Munich
     organizations:
       - ATIX
     provider: vmware
-    provider_auth:
+    provider_params:
       url: vsphere.example.com
       user: admin
       password: secret
-    server_url: vsphere.example.com
+      datacenter: ax01
+    server_url: foreman.example.com
+    username: admin
+    password: secret
+    state: present
+
+- name: ovirt compute resource
+  foreman_compute_resource:
+    name: ovirt_compute_resource
+    locations:
+      - France/Toulouse
+    organizations:
+      - Example Org
+    provider: ovirt
+    provider_params:
+      url: ovirt.example.com
+      user: ovirt-admin@example.com
+      password: ovirtsecret
+      datacenter: aa92fb54-0736-4066-8fa8-b8b9e3bd75ac
+      ovirt_quota: 24868ab9-c2a1-47c3-87e7-706f17d215ac
+      use_v4: true
+    server_url: foreman.example.com
     username: admin
     password: secret
     state: present
@@ -98,115 +128,81 @@ EXAMPLES = '''
 RETURN = ''' # '''
 
 try:
-    import nailgun.entity_mixins
-    import nailgun.entities
-    import nailgun.entity_fields
-    import ansible.module_utils.ansible_nailgun_cement as cement
-    has_import_error = False
+    from ansible.module_utils.ansible_nailgun_cement import (
+        find_compute_resource,
+        find_organizations,
+        find_locations,
+        naildown_entity_state,
+        sanitize_entity_dict,
+    )
 
+    from nailgun.entities import (
+        AbstractComputeResource,
+        LibvirtComputeResource,
+        OVirtComputeResource,
+        VMWareComputeResource,
+    )
+
+    has_import_error = False
 except ImportError as e:
     has_import_error = True
     import_error_msg = str(e)
 
-from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.foreman_helper import ForemanEntityAnsibleModule
+
+# This is the only true source for names (and conversions thereof)
+name_map = {
+    'name': 'name',
+    'description': 'description',
+    'provider': 'provider',
+    'organizations': 'organization',
+    'locations': 'location',
+    'datacenter': 'datacenter',
+}
 
 
-def get_provider_params(provider):
+def get_provider_infos(provider):
     provider_name = provider.lower()
 
     if provider_name == 'libvirt':
         return {
-            'credentials': ['url'],
-            'params': ['display_type'],
-            'class': nailgun.entities.LibvirtComputeResource
+            'params': ['url', 'display_type'],
+            'class': LibvirtComputeResource
         }
 
     elif provider_name == 'ovirt':
         return {
-            'credentials': ['url', 'user', 'password'],
-            'params': [],
-            'class': cement.OVirtComputeResource
+            'params': ['url', 'user', 'password', 'datacenter', 'use_v4', 'ovirt_quota'],
+            'class': OVirtComputeResource
         }
 
     elif provider_name == 'vmware':
         return {
-            'credentials': ['url', 'user', 'password'],
-            'params': ['datacenter'],
-            'class': cement.VMWareComputeResource
+            'params': ['url', 'user', 'password', 'datacenter'],
+            'class': VMWareComputeResource
         }
 
     else:
-        return False
+        return {
+            'params': [],
+            'class': AbstractComputeResource
+        }
 
 
-def main(module):
-    if has_import_error:
-        module.fail_json(msg=import_error_msg)
-
-    name = module.params.get('name')
-    state = module.params.get('state')
-    provider = module.params.get('provider').title()
-    description = module.params.get('description')
-    locations = module.params.get('locations')
-    organizations = module.params.get('organizations')
-
-    cement.create_server(
-        server_url=module.params.get('server_url'),
-        auth=(module.params.get('username'), module.params.get('password')),
-        verify_ssl=module.params.get('verify_ssl'),
-    )
-
-    cement.ping_server(module)
-
-    data = {
-        'name': name,
-        'description': description
-    }
-
-    compute_resource = cement.find_compute_resource(module, name=data.get('name'), failsafe=True)
-
-    if organizations:
-        data['organization'] = [cement.find_organization(module, organization) for organization in organizations]
-
-    if locations:
-        data['location'] = [cement.find_location(module, location) for location in locations]
-
-    data['provider'] = provider
-    provider_params = get_provider_params(provider=provider)
-
-    if state in ['present', 'present_with_defaults']:
-        if not provider_params and not compute_resource:
-            module.fail_json(msg='To create a compute resource a valid provider must be supplied')
-
-        for key in provider_params.get('credentials'):
-            data.__setitem__(key, module.params.get('provider_auth').get(key))
-
-        for key in provider_params.get('params'):
-            if key not in module.params:
-                module.fail_json(msg='missing required param {}'.format(key))
-
-            data.__setitem__(key, module.params.get(key))
-
-    changed = cement.naildown_entity_state(provider_params.get('class'), data, compute_resource, state, module)
-
-    return changed
-
-
-if __name__ == '__main__':
-    module = AnsibleModule(
+def main():
+    module = ForemanEntityAnsibleModule(
         argument_spec=dict(
             name=dict(type='str', required=True),
+            updated_name=dict(type='str'),
             description=dict(type='str'),
             provider=dict(type='str', choices=['vmware', 'libvirt', 'ovirt']),
-            provider_auth=dict(type='dict'),
+            provider_params=dict(type='dict'),
             locations=dict(type='list'),
             organizations=dict(type='list'),
-            server_url=dict(type='str'),
-            username=dict(required=True),
-            password=dict(required=True, no_log=True),
-            verify_ssl=dict(type='bool', default=True),
             state=dict(type='str', default='present', choices=['present', 'absent', 'present_with_defaults']),
-            # provider-params
+
+            # Deprecated provider's specific params, use nested keys in provider_params param instead
+            provider_auth=dict(type='dict'),
             url=dict(type='str'),
             display_type=dict(type='str'),
             datacenter=dict(type='str'),
@@ -216,5 +212,54 @@ if __name__ == '__main__':
         ),
         supports_check_mode=True,
     )
-    result = main(module)
-    module.exit_json(changed=result)
+
+    (entity_dict, state) = module.parse_params()
+
+    if 'provider' in entity_dict:
+        entity_dict['provider'] = entity_dict['provider'].title()
+
+    provider_infos = get_provider_infos(provider=entity_dict.get('provider', ''))
+    provider_params = entity_dict.pop('provider_params', dict())
+    provider_auth = entity_dict.pop('provider_auth', dict())
+
+    module.connect()
+
+    try:
+        # Try to find the compute_resource to work on
+        entity = find_compute_resource(module, name=entity_dict['name'], failsafe=True)
+    except Exception as e:
+        module.fail_json(msg='Failed to find entity: %s ' % e)
+
+    if 'updated_name' in entity_dict and state == 'present':
+        entity_dict['name'] = entity_dict['updated_name']
+
+    if 'organizations' in entity_dict:
+        entity_dict['organizations'] = find_organizations(module, entity_dict['organizations'])
+
+    if 'locations' in entity_dict:
+        entity_dict['locations'] = find_locations(module, entity_dict['locations'])
+
+    entity_dict = sanitize_entity_dict(entity_dict, name_map)
+
+    # Add provider specific params
+    if state in ['present', 'present_with_defaults']:
+        if not provider_infos and not entity:
+            module.fail_json(msg='To create a compute resource a valid provider must be supplied')
+
+        for key in provider_infos['params']:
+            # Manage deprecated params
+            if key in provider_auth:
+                entity_dict[key] = provider_auth[key]
+
+            if key in provider_params:
+                entity_dict[key] = provider_params[key]
+
+    changed = naildown_entity_state(provider_infos['class'], entity_dict, entity, state, module)
+
+    module.exit_json(changed=changed)
+
+
+if __name__ == '__main__':
+    changed = main()
+
+#  vim: set sts=4 ts=8 sw=4 ft=python et noro norl cin si ai :
