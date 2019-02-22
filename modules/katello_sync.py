67e4d4d97ae89c1e939505883aeaafae8d0ca20c
Matthias Dellweg <dellweg@atix.de>
2019-02-22 12:27:53 +0100

    DRY up katello_sync

--- modules/katello_sync.py
+++ modules/katello_sync.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # (c) 2016, Eric D Helms <ericdhelms@gmail.com>
+# (c) 2019, Matthias M Dellweg <dellweg@atix.de>
 #
 # This file is part of Ansible
 #
@@ -22,45 +23,40 @@ DOCUMENTATION = '''
 module: katello_sync
 short_description: Sync a Katello repository or product
 description:
-    - Sync a Katello repository or product
-author: "Eric D Helms (@ehelms)"
+  - Sync a Katello repository or product
+author:
+  - "Eric D Helms (@ehelms)"
+  - "Matthias M Dellweg (@mdellweg) ATIX AG"
 requirements:
-    - "nailgun >= 0.28.0"
-    - "python >= 2.6"
-    - "ansible >= 2.3"
+  - "nailgun >= 0.28.0"
 options:
+  organization:
+    description: Organization that the I(product) is in
+    required: true
+  product:
+    description: Product to which the I(repository) lives in
+    required: true
+  repository:
+    description: |
+      Name of the repository to sync
+      If omitted, all repositories in I(product) are synched.
+  synchronous:
+    description: Wait for the Sync task to complete if True. Immediately return if False.
+    default: true
   server_url:
-    description:
-      - URL of Foreman server
+    description: foreman url
     required: true
   username:
-    description:
-      - Username on Foreman server
+    description: foreman username
     required: true
   password:
-    description:
-      - Password for user accessing Foreman server
+    description: foreman user password
     required: true
   verify_ssl:
-    description:
-      - Verify SSL of the Foreman server
+    description: verify ssl connection when communicating with foreman
     default: true
     type: bool
-  repository:
-    description:
-      - Name of the repository to sync
-  product:
-    description:
-      - Product to which the repository lives in
-    required: true
-  synchronous:
-    description:
-      - Wait for the Sync task to complete if True. Immediately return if False.
-    default: true
-  organization:
-    description:
-      - Organization that the Product is in
-    required: true
+...
 '''
 
 EXAMPLES = '''
@@ -107,115 +103,55 @@ EXAMPLES = '''
   until: async_job_result.finished
   retries: 999
   delay: 10
-
 '''
 
-RETURN = '''# '''
+RETURN = ''' # '''
 
 try:
-    from nailgun import entities, entity_fields, entity_mixins
-    from nailgun.config import ServerConfig
-    has_import_error = False
-except ImportError as e:
-    has_import_error = True
-    import_error_msg = str(e)
+    from ansible.module_utils.ansible_nailgun_cement import (
+        find_organization,
+        find_product,
+        find_repository,
+    )
 
-from ansible.module_utils.basic import AnsibleModule
+    from nailgun import entity_mixins
+    entity_mixins.TASK_TIMEOUT = 180000  # Publishes can sometimes take a long, long time
+except ImportError:
+    pass
 
-
-class NailGun(object):
-
-    def __init__(self, server, entities, module):
-        self._server = server
-        self._entities = entities
-        self._module = module
-        entity_mixins.TASK_TIMEOUT = 180000  # Publishes can sometimes take a long, long time
-
-    def find_organization(self, name):
-        org = self._entities.Organization(self._server, name=name)
-        response = org.search(set(), {'search': 'name="{}"'.format(name)})
-
-        if len(response) == 1:
-            return response[0]
-        else:
-            self._module.fail_json(msg="No organization found for %s" % name)
-
-    def find_product(self, name, organization):
-        org = self.find_organization(organization)
-
-        product = self._entities.Product(self._server, name=name, organization=org)
-        response = product.search()
-
-        if len(response) == 1:
-            return response[0]
-        else:
-            self._module.fail_json(msg="No Product found for %s" % name)
-
-    def find_repository(self, name, product, organization):
-        product = self.find_product(product, organization)
-
-        repository = self._entities.Repository(self._server, name=name, product=product)
-        repository._fields['organization'] = entity_fields.OneToOneField(entities.Organization)
-        repository.organization = product.organization
-        response = repository.search()
-
-        if len(response) == 1:
-            return response[0]
-        else:
-            self._module.fail_json(msg="No Repository found for %s" % name)
-
-    def sync(self, product, organization, synchronous, repository=None):
-        if repository is None:
-            product = self.find_product(product, organization)
-            return product.sync(synchronous)
-        else:
-            repository = self.find_repository(repository, product, organization)
-            return repository.sync(synchronous)
+from ansible.module_utils.foreman_helper import ForemanAnsibleModule
 
 
 def main():
-    module = AnsibleModule(
+    module = ForemanAnsibleModule(
         argument_spec=dict(
-            server_url=dict(required=True),
-            username=dict(required=True, no_log=True),
-            password=dict(required=True, no_log=True),
-            verify_ssl=dict(type='bool', default=True),
-            product=dict(required=True),
-            synchronous=dict(type='bool', default=True),
             organization=dict(required=True),
+            product=dict(required=True),
             repository=dict(),
+            synchronous=dict(type='bool', default=True),
         ),
         supports_check_mode=False,
     )
 
-    if has_import_error:
-        module.fail_json(msg=import_error_msg)
+    params = module.parse_params()
 
-    server_url = module.params['server_url']
-    verify_ssl = module.params['verify_ssl']
-    username = module.params['username']
-    password = module.params['password']
-    repository = module.params['repository']
-    product = module.params['product']
-    synchronous = module.params['synchronous']
-    organization = module.params['organization']
+    module.connect()
 
-    server = ServerConfig(
-        url=server_url,
-        auth=(username, password),
-        verify=verify_ssl
-    )
-    ng = NailGun(server, entities, module)
-
-    # Lets make an connection to the server with username and password
     try:
-        org = entities.Organization(server)
-        org.search()
+        organization = find_organization(module, params['organization'])
+        product = find_product(module, params['product'], organization)
+        if 'repository' in params:
+            repository = find_repository(module, params['repository'], product)
+        else:
+            repository = None
     except Exception as e:
-        module.fail_json(msg="Failed to connect to Foreman server: %s " % e)
+        module.fail_json(msg='Failed to find entity: %s ' % e)
 
     try:
-        changed = ng.sync(product, organization, synchronous, repository)
+        if repository is None:
+            changed = product.sync(params['synchronous'])
+        else:
+            changed = repository.sync(params['synchronous'])
         module.exit_json(changed=changed)
     except Exception as e:
         module.fail_json(msg=e)
