9f9d04510e4428e54021f79359cda72e57f25189
Evgeni Golov <evgeni@golov.de>
2019-05-20 15:58:59 +0200

    migrate to ansible-runner for tests (#272)
    this fixes ansible 2.8 problems too

--- test/test_crud.py
+++ test/test_crud.py
@@ -2,11 +2,7 @@ import pytest
 import os
 import tempfile
 import json
-from ansible.utils.display import Display
-import __main__
-__main__.display = Display()
-from ansible.cli.playbook import PlaybookCLI
-
+import ansible_runner
 
 MODULES = [
     'activation_key',
@@ -42,19 +38,19 @@ MODULES = [
 ]
 
 
-def run_playbook_vcr(module, extra_vars=None, extra_args=None, record=False):
-    # Assemble extra parameters for playbook call
-    if extra_args is None:
-        extra_args = []
+def run_playbook_vcr(module, extra_vars=None, record=False):
+    if extra_vars is None:
+        extra_vars = {}
+    limit = None
     if record:
         # Cassettes that are to be overwritten must be deleted first
         record_mode = 'once'
-        extra_args.extend(['--extra-vars', 'recording=true'])
+        extra_vars['recording'] = True
     else:
         # Never reach out to the internet
         record_mode = 'none'
         # Only run the tests (skip fixtures)
-        extra_args.extend(['--limit', '!fixtures'])
+        limit = '!fixtures'
 
     # Dump recording parameters to json-file and pass its name by environment
     test_params = {'test_name': module, 'serial': 0, 'record_mode': record_mode}
@@ -62,23 +58,20 @@ def run_playbook_vcr(module, extra_vars=None, extra_args=None, record=False):
         json.dump(test_params, params_file.file)
         params_file.file.close()
         os.environ['FAM_TEST_VCR_PARAMS_FILE'] = params_file.name
-        return run_playbook(module, extra_vars=extra_vars, extra_args=extra_args)
+        return run_playbook(module, extra_vars=extra_vars, limit=limit)
 
 
-def run_playbook(module, extra_vars=None, extra_args=None):
+def run_playbook(module, extra_vars=None, limit=None):
     # Assemble parameters for playbook call
-    path = 'test/test_playbooks/{}.yml'.format(module)
-    playbook_opts = ['ansible-playbook', '-vvvv', path]
-    if extra_vars:
-        playbook_opts.extend(['--extra-vars', extra_vars])
-    if extra_args:
-        playbook_opts.extend(extra_args)
-
-    cli = PlaybookCLI(playbook_opts)
-    cli.parse()
-    return cli.run()
+    os.environ['ANSIBLE_CONFIG'] = os.path.join(os.getcwd(), 'ansible.cfg')
+    playbook_path = os.path.join(os.getcwd(), 'test', 'test_playbooks', '{}.yml'.format(module))
+    inventory_path = os.path.join(os.getcwd(), 'test', 'inventory', 'hosts')
+    run = ansible_runner.run(extravars=extra_vars, playbook=playbook_path, limit=limit, verbosity=4, inventory=inventory_path)
+    return run
 
 
 @pytest.mark.parametrize('module', MODULES)
 def test_crud(module, record):
-    assert run_playbook_vcr(module, record=record) == 0
+    run = run_playbook_vcr(module, record=record)
+    print(run.stdout.read())
+    assert run.rc == 0
