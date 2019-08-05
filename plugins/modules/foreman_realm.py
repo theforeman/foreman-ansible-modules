0ea9ab22d0f860efbd4fd17735c342473a498505
Matthias Dellweg <dellweg@atix.de>
2019-08-05 09:22:58 +0200

    Port foreman_realmto apypie
    * Make it idempotent

--- plugins/modules/foreman_realm.py
+++ plugins/modules/foreman_realm.py
@@ -26,36 +26,15 @@ description:
 author:
   - "Lester R Claudio (@claudiol1)"
 requirements:
-  - "nailgun >= 0.30.2"
+  - apypie
 options:
-  server_url:
-    description:
-      - URL of Foreman server
-    required: true
-  username:
-    description:
-      - Username on Foreman server
-    required: true
-  password:
-    description:
-      - Password for user accessing Foreman server
-    required: true
-  validate_certs:
-    aliases:
-      - verify_ssl
-    description:
-      - Verify SSL of the Foreman server
-    required: false
-    default: true
-    type: bool
   name:
     description:
       - Name of the Foreman realm
     required: true
   realm_proxy:
     description:
-      - Id of Proxy to use for this realm
-    type: int
+      - Proxy to use for this realm
     required: true
   realm_type:
     description:
@@ -68,7 +47,9 @@ options:
     choices:
       - present
       - absent
+extends_documentation_fragment: foreman
 '''
+
 EXAMPLES = '''
 - name: "Create EXAMPLE.LOCAL Realm"
   foreman_realm:
@@ -76,92 +57,35 @@ EXAMPLES = '''
     password: "changeme"
     server_url: "https://foreman.example.com"
     name: "EXAMPLE.COM"
-    realm_proxy: 1
+    realm_proxy: "foreman.example.com"
     realm_type: "Red Hat Identity Management"
     state: present
 '''
 
 RETURN = ''' # '''
 
-try:
-    from ansible.module_utils.ansible_nailgun_cement import (
-        create_server,
-        ping_server,
-        entity_mixins,
-        find_entities,
-        naildown_entity_state,
-    )
-    from nailgun.entities import Realm
-
-    has_import_error = False
-except ImportError as e:
-    has_import_error = True
-    import_error_msg = str(e)
-
-
-from ansible.module_utils.basic import AnsibleModule
-
-
-def sanitize_realm_dict(realm_dict):
-    # This is the only true source for names (and conversions thereof)
-    name_map = {
-        'name': 'name',
-        'realm_proxy': 'realm_proxy',
-        'realm_type': 'realm_type',
-    }
-    result = {}
-    for key, value in name_map.items():
-        if key in realm_dict:
-            result[value] = realm_dict[key]
-    return result
+from ansible.module_utils.foreman_helper import ForemanEntityApypieAnsibleModule
 
 
 def main():
-
-    module = AnsibleModule(
-        argument_spec=dict(
-            server_url=dict(required=True),
-            username=dict(required=True, no_log=True),
-            password=dict(required=True, no_log=True),
-            validate_certs=dict(type='bool', default=True, aliases=['verify_ssl']),
+    module = ForemanEntityApypieAnsibleModule(
+        entity_spec=dict(
             name=dict(required=True),
-            realm_proxy=dict(type='int', required=True),
+            realm_proxy=dict(type='entity', flat_name='realm_proxy_id', required=True),
             realm_type=dict(required=True, choices=['Red Hat Identity Management', 'FreeIPA', 'Active Directory']),
-            state=dict(default='present', choices=['present', 'absent']),
         ),
-        supports_check_mode=True,
     )
 
-    if has_import_error:
-        module.fail_json(msg=import_error_msg)
+    entity_dict = module.clean_params()
 
-    realm_dict = dict(
-        [(k, v) for (k, v) in module.params.items() if v is not None])
+    module.connect()
 
-    server_url = realm_dict.pop('server_url')
-    username = realm_dict.pop('username')
-    password = realm_dict.pop('password')
-    verify_ssl = realm_dict.pop('validate_certs')
-    state = realm_dict.pop('state')
+    entity = module.find_resource_by_name('realms', name=entity_dict['name'], failsafe=True)
 
-    try:
-        create_server(server_url, (username, password), verify_ssl)
-    except Exception as e:
-        module.fail_json(msg="Failed to connect to Foreman server: %s " % e)
+    if not module.desired_absent:
+        entity_dict['realm_proxy'] = module.find_resource_by_name('smart_proxies', entity_dict['realm_proxy'], thin=True)
 
-    # ping_server(module)
-    try:
-        entities = find_entities(Realm, name=realm_dict['name'])
-        if len(entities) > 0:
-            entity = entities[0]
-        else:
-            entity = None
-    except Exception as e:
-        module.fail_json(msg='Failed to find entity: %s ' % e)
-
-    realm_dict = sanitize_realm_dict(realm_dict)
-
-    changed = naildown_entity_state(Realm, realm_dict, entity, state, module)
+    changed = module.ensure_entity_state('realms', entity_dict, entity)
 
     module.exit_json(changed=changed)
 
