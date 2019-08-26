1b806e6d465ca3acd3c15e59fe98a1ea5dd6818a
Evgeni Golov <evgeni@golov.de>
2019-08-26 12:30:08 +0200

    patch apidoc to include rex proxy ids for subnets (#430)
    * patch apidoc to include rex proxy ids for subnets
    
    Fixes: #428
    
    * don't patch on systems without foreman_remote_execution

--- plugins/module_utils/foreman_helper.py
+++ plugins/module_utils/foreman_helper.py
@@ -199,6 +199,38 @@ class ForemanAnsibleModule(ForemanBaseAnsibleModule):
         _location_update_params_location = next(x for x in _location_update['params'] if x['name'] == 'location')
         _location_update_params_location['params'].append(_location_organizations_parameter)
 
+    def _patch_subnet_rex_api(self):
+        """This is a workaround for the broken subnet apidoc in foreman remote execution.
+            see https://projects.theforeman.org/issues/19086
+        """
+
+        if 'remote_execution_features' not in self.foremanapi.apidoc['docs']['resources']:
+            # the system has no foreman_remote_execution installed, no need to patch
+            return
+
+        _subnet_rex_proxies_parameter = {
+            u'validations': [],
+            u'name': u'remote_execution_proxy_ids',
+            u'show': True,
+            u'description': u'\n<p>Remote Execution Proxy IDs</p>\n',
+            u'required': False,
+            u'allow_nil': True,
+            u'allow_blank': False,
+            u'full_name': u'subnet[remote_execution_proxy_ids]',
+            u'expected_type': u'array',
+            u'metadata': None,
+            u'validator': u'',
+        }
+        _subnet_methods = self.foremanapi.apidoc['docs']['resources']['subnets']['methods']
+
+        _subnet_create = next(x for x in _subnet_methods if x['name'] == 'create')
+        _subnet_create_params_subnet = next(x for x in _subnet_create['params'] if x['name'] == 'subnet')
+        _subnet_create_params_subnet['params'].append(_subnet_rex_proxies_parameter)
+
+        _subnet_update = next(x for x in _subnet_methods if x['name'] == 'update')
+        _subnet_update_params_subnet = next(x for x in _subnet_update['params'] if x['name'] == 'subnet')
+        _subnet_update_params_subnet['params'].append(_subnet_rex_proxies_parameter)
+
     def check_requirements(self):
         if not HAS_APYPIE:
             self.fail_json(msg='The apypie Python module is required', exception=APYPIE_IMP_ERR)
@@ -214,6 +246,7 @@ class ForemanAnsibleModule(ForemanBaseAnsibleModule):
         )
 
         self._patch_location_api()
+        self._patch_subnet_rex_api()
 
         if ping:
             self.ping()
