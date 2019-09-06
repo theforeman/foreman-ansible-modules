0fd87529b67d3d925a9b17ae0bf1abbe4164b998
Evgeni Golov <evgeni@golov.de>
2019-09-06 14:23:26 +0200

    add/update ansible_metadata for all modules

--- plugins/modules/foreman_location.py
+++ plugins/modules/foreman_location.py
@@ -17,6 +17,10 @@
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
 DOCUMENTATION = '''
 ---
 module: foreman_location
