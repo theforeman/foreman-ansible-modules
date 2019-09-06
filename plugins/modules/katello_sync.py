93d317c232d923a2cad227d6361d689e7a66942a
Evgeni Golov <evgeni@golov.de>
2019-09-06 15:35:08 +0200

    add/update ansible_metadata for all modules (#454)
    add/update ansible_metadata for all modules

--- plugins/modules/katello_sync.py
+++ plugins/modules/katello_sync.py
@@ -18,6 +18,10 @@
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
 DOCUMENTATION = '''
 ---
 module: katello_sync
