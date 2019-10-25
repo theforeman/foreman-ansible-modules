98c838fadd106fdfd19c1420044ba62ce413ff77
Evgeni Golov <evgeni@golov.de>
2019-10-25 12:35:49 +0200

    allow certain resource_action calls in check_mode (#555)
    some calls are read only, but we don't use the find/list wrappers
    because the api is weird. let's allow these.
    
    otherwise e.g. katello_activation_key is broken in check mode when
    trying to assign content overrides

--- plugins/modules/foreman_host_power.py
+++ plugins/modules/foreman_host_power.py
@@ -115,7 +115,7 @@ def main():
     module.connect()
 
     params = {'id': entity_dict['name']}
-    _power_state_changed, power_state = module.resource_action('hosts', 'power_status', params=params)
+    _power_state_changed, power_state = module.resource_action('hosts', 'power_status', params=params, ignore_check_mode=True)
     if module.state in ['state', 'status']:
         module.exit_json(changed=False, power_state=power_state['state'])
     elif ((module.state in ['on', 'start'] and power_state['state'] == 'on')
