49adff7fd1baee6c9da21033e64f469686ed899f
Evgeni Golov <evgeni@golov.de>
2019-11-15 13:41:57 +0100

    don't treat pycache as a module in test_module_state (#594)
    * don't treat pycache as a module in test_module_state
    
    * better matching

--- tests/test_module_state.py
+++ tests/test_module_state.py
@@ -18,9 +18,9 @@ MODULES_PATH = py.path.local(__file__).realpath() / '..' / '..' / 'plugins' / 'm
 
 def find_all_modules():
     for module in MODULES_PATH.listdir(sort=True):
-        module = module.basename.replace('.py', '')
-        if module != '__init__':
-            yield module
+        module = module.basename
+        if module.endswith('.py') and not module.startswith('_'):
+            yield module.replace('.py', '')
 
 
 ALL_MODULES = list(find_all_modules())
