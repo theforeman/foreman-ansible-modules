6b529eeef73a03389f3fc32e3787cf45faf42235
Matthias Dellweg <dellweg@atix.de>
2019-12-12 16:49:18 +0100

    Document returned entity in foreman_bookmark

--- plugins/modules/bookmark.py
+++ plugins/modules/bookmark.py
@@ -85,7 +85,39 @@ EXAMPLES = '''
     state: absent
 '''
 
-RETURN = ''' # '''
+RETURN = '''
+entity:
+  description: Final state of the affected entities grouped by their type.
+  returned: success
+  type: dict
+  contains:
+    bookmarks:
+      description: List of bookmarks.
+      type: list
+      elements: dict
+      contains:
+        id:
+          description: Database id of the bookmark.
+          type: int
+        name:
+          description: Name of the bookmark.
+          type: str
+        controller:
+          description: Controller, the query is performed on.
+          type: str
+        query:
+          description: Query to be performed on the controller.
+          type: str
+        public:
+          description: Publicity of the bookmark.
+          type: bool
+        owner_type:
+          description: Class of the owner entity.
+          type: str
+        owner_id:
+          description: Database id of the owner entity.
+          type: int
+'''
 
 from ansible_collections.theforeman.foreman.plugins.module_utils.foreman_helper import ForemanEntityAnsibleModule
 
