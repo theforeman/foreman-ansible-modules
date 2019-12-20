4f86396fe77c0219baaed830820ddb1c9c7ff237
Evgeni Golov <evgeni@golov.de>
2019-12-20 11:24:32 +0100

    don't force sub items to be "text", Ansible already does so for us

--- plugins/modules/katello_repository_set.py
+++ plugins/modules/katello_repository_set.py
@@ -140,13 +140,11 @@ EXAMPLES = '''
 RETURN = ''' # '''
 
 from ansible.module_utils.foreman_helper import KatelloEntityAnsibleModule
-from ansible.module_utils._text import to_text
 
 
 def get_desired_repos(desired_substitutions, available_repos):
     desired_repos = []
     for sub in desired_substitutions:
-        sub = {to_text(k): to_text(v) for (k, v) in sub.items()}
         desired_repos += filter(lambda available: available['substitutions'] == sub, available_repos)
     return desired_repos
 
