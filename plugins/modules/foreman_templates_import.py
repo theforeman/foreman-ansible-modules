ac2d953c0ae821e75515b4aea651057cb03abb0b
Anton Nesterov <nesanton@redhat.com>
2020-04-01 12:46:40 +0200

    Fix after removed clean_params

--- plugins/modules/foreman_templates_import.py
+++ plugins/modules/foreman_templates_import.py
@@ -121,7 +121,7 @@ def main():
         supports_check_mode=False,
     )
 
-    module_params = module.clean_params()
+    module_params = module.foreman_params
     module.connect()
 
     if 'template' in module.foremanapi.resources:
