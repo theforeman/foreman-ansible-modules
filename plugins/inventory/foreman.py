0cd30256c230d6c7d9278992e962fc7eaff3aa32
Ondřej Ezr <oezr@redhat.com>
2020-08-25 14:25:41 +0200

    Include blank parent groups
    Fixes #919

--- plugins/inventory/foreman.py
+++ plugins/inventory/foreman.py
@@ -235,9 +235,16 @@ class InventoryModule(BaseInventoryPlugin, Cacheable, Constructable):
                 # create directly mapped groups
                 group_name = host.get('hostgroup_title', host.get('hostgroup_name'))
                 if group_name:
-                    group_name = to_safe_group_name('%s%s' % (self.get_option('group_prefix'), group_name.lower().replace(" ", "")))
-                    group_name = self.inventory.add_group(group_name)
-                    self.inventory.add_child(group_name, host_name)
+                    parent_name = None
+                    group_label_parts = []
+                    for part in group_name.split('/'):
+                        group_label_parts.append(part.lower().replace(" ", ""))
+                        gname = to_safe_group_name('%s%s' % (self.get_option('group_prefix'), '/'.join(group_label_parts)))
+                        result_gname = self.inventory.add_group(gname)
+                        if parent_name:
+                            self.inventory.add_child(parent_name, result_gname)
+                        parent_name = result_gname
+                    self.inventory.add_child(result_gname, host_name)
 
                 if self.get_option('legacy_hostvars'):
                     hostvars = self._get_hostvars(host)
