9edb119cc2290ecb673e05087746815e7c494570
Evgeni Golov <evgeni@golov.de>
2020-10-02 12:28:19 +0200

    also add id to filter spec of role module, even if unused currently

--- plugins/modules/role.py
+++ plugins/modules/role.py
@@ -95,6 +95,7 @@ from ansible_collections.theforeman.foreman.plugins.module_utils.foreman_helper 
 
 
 filter_foreman_spec = dict(
+    id=dict(invisible=True),
     permissions=dict(type='entity_list', required=True),
     search=dict(),
 )
