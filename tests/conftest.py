deb67373e7b2f562ddd9e9e05a8967ba5e187c63
Evgeni Golov <evgeni@golov.de>
2020-10-05 11:21:13 +0200

    autodetect all test playbooks

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,70 +1,18 @@
 import pytest
+import py.path
 
 
-TEST_PLAYBOOKS = [
-    'activation_key',
-    'architecture',
-    'auth_source_ldap',
-    'bookmark',
-    'compute_attribute',
-    'compute_profile',
-    'compute_resource',
-    'config_group',
-    'content_credential',
-    'content_upload',
-    'content_view',
-    'content_view_filter',
-    'content_view_version',
-    'domain',
-    'external_usergroup',
-    'filters',
-    'global_parameter',
-    'hardware_model',
-    'host',
-    'host_collection',
-    'host_power',
-    'hostgroup',
-    'http_proxy',
-    'image',
-    'installation_medium',
-    'inventory_plugin',
-    'job_invocation',
-    'job_template',
-    'katello_hostgroup',
-    'katello_manifest',
-    'katello_smart_proxy',
-    'lifecycle_environment',
-    'location',
-    'luna_hostgroup',
-    'operatingsystem',
-    'organization',
-    'os_default_template',
-    'partition_table',
-    'product',
-    'provisioning_template',
-    'puppet_environment',
-    'realm',
-    'redhat_manifest',
-    'repository',
-    'repository_set',
-    'repository_sync',
-    'resource_info',
-    'role',
-    'scap_content',
-    'scap_tailoring_file',
-    'scc_account',
-    'scc_product',
-    'setting',
-    'smart_class_parameter',
-    'smart_proxy',
-    'snapshot',
-    'status_info',
-    'subnet',
-    'sync_plan',
-    'templates_import',
-    'user',
-    'usergroup',
-]
+TEST_PLAYBOOKS_PATH = py.path.local(__file__).realpath() / '..' / 'test_playbooks'
+
+
+def find_all_test_playbooks():
+    for playbook in TEST_PLAYBOOKS_PATH.listdir(sort=True):
+        playbook = playbook.basename
+        if playbook.endswith('.yml'):
+            yield playbook.replace('.yml', '')
+
+
+TEST_PLAYBOOKS = list(find_all_test_playbooks())
 
 
 def pytest_addoption(parser):
