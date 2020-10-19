564614f99d596c8372dee2fbd3f67eb766458e05
Jeffrey van Pelt <jeff@vanpelt.one>
2020-10-19 13:40:15 +0200

    Better, more clear name for variable

--- plugins/modules/smart_proxy.py
+++ plugins/modules/smart_proxy.py
@@ -127,11 +127,11 @@ def main():
         payload = {
             'id': smart_proxy['id'],
         }
-        capsule_content = module.resource_action('capsule_content', 'lifecycle_environments', payload, record_change=False)
+        current_lces = module.resource_action('capsule_content', 'lifecycle_environments', payload, record_change=False)
 
         if handle_lifecycle_environments:
             desired_environment_ids = set(lifecycle_environment['id'] for lifecycle_environment in lifecycle_environments)
-            current_environment_ids = set(lifecycle_environment['id'] for lifecycle_environment in capsule_content['results']) if capsule_content else set()
+            current_environment_ids = set(lifecycle_environment['id'] for lifecycle_environment in current_lces['results']) if current_lces else set()
 
             module.record_before('smart_proxy_content/lifecycle_environment_ids', current_environment_ids)
             module.record_after('smart_proxy_content/lifecycle_environment_ids', desired_environment_ids)
