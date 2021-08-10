3f4b650fa4f2f5d6f821d9a48cb067b913464c17
Evgeni Golov <evgeni@golov.de>
2021-08-10 13:17:44 +0200

    don't accidentally duplicate `kt_activation_keys` param
    Fixes: #1268

--- plugins/module_utils/foreman_helper.py
+++ plugins/module_utils/foreman_helper.py
@@ -325,9 +325,10 @@ class HostMixin(ParametersMixin):
                 ak_param = {'name': 'kt_activation_keys', 'parameter_type': 'string', 'value': self.foreman_params.pop('activation_keys')}
                 self.foreman_params['parameters'] = parameters + [ak_param]
             elif 'parameters' in self.foreman_params and entity is not None:
-                ak_param = next((param for param in entity.get('parameters') if param['name'] == 'kt_activation_keys'), None)
-                if ak_param:
-                    self.foreman_params['parameters'].append(ak_param)
+                current_ak_param = next((param for param in entity.get('parameters') if param['name'] == 'kt_activation_keys'), None)
+                desired_ak_param = next((param for param in self.foreman_params['parameters'] if param['name'] == 'kt_activation_keys'), None)
+                if current_ak_param and desired_ak_param is None:
+                    self.foreman_params['parameters'].append(current_ak_param)
 
         self.validate_parameters()
 
