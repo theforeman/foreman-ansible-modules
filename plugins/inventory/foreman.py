d73c1b1d995c789b5578888b3b0917ddd86f0694
Evgeni Golov <evgeni@golov.de>
2021-09-30 13:55:54 +0200

    enable certificate validation by default for the inventory

--- plugins/inventory/foreman.py
+++ plugins/inventory/foreman.py
@@ -51,7 +51,7 @@ DOCUMENTATION = '''
         description:
           - Whether or not to verify the TLS certificates of the Foreman server.
         type: boolean
-        default: False
+        default: True
         env:
             - name: FOREMAN_VALIDATE_CERTS
       group_prefix:
