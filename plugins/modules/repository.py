49e2aa19ab98e39e90f169ffbf075aeecfd4c7d4
Evgeni Golov <evgeni@golov.de>
2022-04-29 10:38:52 +0200

    repository - add rhel-9 to os version filter choices

--- plugins/modules/repository.py
+++ plugins/modules/repository.py
@@ -222,6 +222,7 @@ options:
       - rhel-6
       - rhel-7
       - rhel-8
+      - rhel-9
   arch:
     description:
       - Architecture of content in the repository
@@ -318,7 +319,7 @@ def main():
             ignorable_content=dict(type='list', elements='str'),
             ansible_collection_requirements=dict(),
             auto_enabled=dict(type='bool'),
-            os_versions=dict(type='list', elements='str', choices=['rhel-6', 'rhel-7', 'rhel-8']),
+            os_versions=dict(type='list', elements='str', choices=['rhel-6', 'rhel-7', 'rhel-8', 'rhel-9']),
             arch=dict(),
         ),
         argument_spec=dict(
