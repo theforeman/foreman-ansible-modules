3fd6accaf73169161b1453d2ddce9f87bccdf9c3
Evgeni Golov <evgeni@golov.de>
2022-11-08 14:24:06 +0100

    add an apidoc patch for missing ignore_types on org controller

--- plugins/module_utils/foreman_helper.py
+++ plugins/module_utils/foreman_helper.py
@@ -545,6 +545,35 @@ class ForemanAnsibleModule(AnsibleModule):
         if next((x for x in _ak_product_content['params'] if x['name'] == 'per_page'), None) is None:
             _ak_product_content['params'].append(_per_page_param)
 
+    @_check_patch_needed(fixed_version='3.5.0', plugins=['katello'])
+    def _patch_organization_ignore_types_api(self):
+        """
+        This is a workaround for the missing ignore_types in the organization apidoc in Katello.
+        See https://projects.theforeman.org/issues/35687
+        """
+
+        _ignore_types_param = {
+            "name": "ignore_types",
+            "full_name": "organization[ignore_types]",
+            "description": "\n<p>List of resources types that will be automatically associated</p>\n",
+            "required": False,
+            "allow_nil": True,
+            "allow_blank": False,
+            "validator": "Must be an array of any type",
+            "expected_type": "array",
+            "metadata": None,
+            "show": True,
+            "validations": []
+        }
+
+        _organization_methods = self.foremanapi.apidoc['docs']['resources']['organizations']['methods']
+
+        _organization_create = next(x for x in _organization_methods if x['name'] == 'create')
+        _organization_update = next(x for x in _organization_methods if x['name'] == 'update')
+        if next((x for x in _organization_create['params'] if x['name'] == 'ignore_types'), None) is None:
+            _organization_create['params'].append(_ignore_types_param)
+            _organization_update['params'].append(_ignore_types_param)
+
     def check_requirements(self):
         if not HAS_APYPIE:
             self.fail_json(msg=missing_required_lib("requests"), exception=APYPIE_IMP_ERR)
@@ -589,6 +618,7 @@ class ForemanAnsibleModule(AnsibleModule):
         self._patch_organization_update_api()
         self._patch_cv_filter_rule_api()
         self._patch_ak_product_content_per_page()
+        self._patch_organization_ignore_types_api()
 
     @_exception2fail_json(msg="Failed to connect to Foreman server: {0}")
     def status(self):
