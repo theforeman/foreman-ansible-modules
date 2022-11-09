70e3e431d767585087c13f1e838bc700ccdafa0a
Manisha15 <singhal@atix.de>
2022-11-09 17:24:21 +0100

    Add module to list all snapshots for a host

--- tests/vcr_python_wrapper.py
+++ tests/vcr_python_wrapper.py
@@ -167,7 +167,7 @@ else:
     if test_params['test_name'] in ['domain', 'hostgroup', 'katello_hostgroup', 'luna_hostgroup', 'realm', 'subnet', 'puppetclasses_import']:
         fam_vcr.register_matcher('query_ignore_proxy', query_matcher_ignore_proxy)
         query_matcher = 'query_ignore_proxy'
-    elif test_params['test_name'] == 'snapshot':
+    elif test_params['test_name'] in ['snapshot', 'snapshot_info']:
         fam_vcr.register_matcher('snapshot_query', snapshot_query_matcher)
         query_matcher = 'snapshot_query'
 
