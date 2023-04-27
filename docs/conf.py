d8f572811133cf9a17b7ef11ca814bf6dcff479c
Evgeni Golov <evgeni@golov.de>
2023-04-27 14:41:26 +0200

    link to python 3 docs instead of 2

--- docs/conf.py
+++ docs/conf.py
@@ -170,4 +170,4 @@ texinfo_documents = [
 # -- Options for intersphinx extension ---------------------------------------
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'python': ('https://docs.python.org/2', None), 'ansible': ('https://docs.ansible.com/ansible/latest/', None)}
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None), 'ansible': ('https://docs.ansible.com/ansible/latest/', None)}
