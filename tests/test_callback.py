9cd309302aed1a6541c9a2d8c4d272339d73b5e2
Evgeni Golov <evgeni@golov.de>
2023-08-22 11:11:50 +0200

    ignore `retries` keyword in callback tests
    its value is sometimes present and sometimes not, based on ansible
    version

--- tests/test_callback.py
+++ tests/test_callback.py
@@ -39,7 +39,7 @@ def drop_incompatible_items(d):
     dd = {}
     for k, v in d.items():
         if k in ['msg', 'start', 'end', 'delta', 'uuid', 'timeout', '_ansible_no_log', 'warn', 'connection',
-                 'extended_allitems', 'loop_control', 'expand_argument_vars']:
+                 'extended_allitems', 'loop_control', 'expand_argument_vars', 'retries']:
             continue
 
         if isinstance(v, dict):
