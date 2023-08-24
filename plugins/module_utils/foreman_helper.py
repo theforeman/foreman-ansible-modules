b018aee27b662033e843d49895662c5af850caa4
Evgeni Golov <evgeni@golov.de>
2023-08-24 08:52:48 +0200

    ensure strings are strings when converting parameters for the API
    We define the `value` attribute as `raw` as it can have different data
    types, which means Ansible will helpfully convert a `"['something']"`
    string to an actual array with the entry `'something'`.
    However, if the `parameter_type` in Foreman is `string`, we really
    should send a string, not an array Ansible converted for us.

--- plugins/module_utils/foreman_helper.py
+++ plugins/module_utils/foreman_helper.py
@@ -1755,7 +1755,7 @@ def _is_resolved(spec, what):
 # Helper for (global, operatingsystem, ...) parameters
 def parameter_value_to_str(value, parameter_type):
     """Helper to convert the value of parameters to string according to their parameter_type."""
-    if parameter_type in ['real', 'integer']:
+    if parameter_type in ['real', 'integer', 'string']:
         parameter_string = str(value)
     elif parameter_type in ['array', 'hash', 'yaml', 'json']:
         parameter_string = json.dumps(value, sort_keys=True)
