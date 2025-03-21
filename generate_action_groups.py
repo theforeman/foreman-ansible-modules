5b5f81de1e3cdf0dc2286343cb78c3e54bfdc53c
Evgeni Golov <evgeni@golov.de>
2025-03-21 10:37:22 +0100

    lint meta/runtime.yml

--- generate_action_groups.py
+++ generate_action_groups.py
@@ -5,10 +5,17 @@ from tests.utils import ALL_MODULES
 META_RUNTIME = 'meta/runtime.yml'
 EXCLUDED_MODULES = ['redhat_manifest']
 
+
+class IndentedListDumper(yaml.SafeDumper):
+
+    def increase_indent(self, flow=False, indentless=False):
+        return super().increase_indent(flow, False)
+
+
 with open(META_RUNTIME) as runtime_file:
     runtime = yaml.safe_load(runtime_file)
 
 runtime['action_groups']['foreman'] = sorted(set(ALL_MODULES) - set(EXCLUDED_MODULES))
 
 with open(META_RUNTIME, 'w') as runtime_file:
-    yaml.safe_dump(runtime, runtime_file, default_flow_style=False, explicit_start=True)
+    yaml.dump(runtime, runtime_file, Dumper=IndentedListDumper, default_flow_style=False, explicit_start=True)
