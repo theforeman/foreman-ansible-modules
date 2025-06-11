db83f9bac0e702eb7ebbf0a55a18f4559334be7b
Mike <mike@hulsman.net>
2025-06-11 14:17:09 +0200

    Update activation_key.py
    Got extra information from  evgeni

--- plugins/modules/activation_key.py
+++ plugins/modules/activation_key.py
@@ -82,7 +82,9 @@ options:
   content_overrides:
     description:
       - List of content overrides that include label and override state
-      - Label refers to repository in the format: <organizationLabel>_<productLabel>_<repositoryLabel>, e.g. ExampleOrg_ExampleProduct_ExampleRepository
+      - Label refers to repository C(content_label).
+      - For Red Hat products for example C(rhel-7-server-rpms).
+      - For custom products it's in the format: C(<organization_label>_<product_label>_<repositoryLabel>), e.g. C(ExampleOrg_ExampleProduct_ExampleRepository).
       - Override state ('enabled', 'disabled', or 'default') sets initial state of repository for newly registered hosts
     type: list
     elements: dict
