9498917cea67f4004d2a5e13731888c8bffad1e1
Evgeni Golov <evgeni@golov.de>
2025-10-06 10:19:19 +0200

    don't record the authorization token in the registration command
    while the token won't work for anyone, this makes security scanners not
    stumble upon this as a false positive leak

--- tests/vcr_python_wrapper.py
+++ tests/vcr_python_wrapper.py
@@ -104,10 +104,25 @@ def job_invocation_body_matcher(r1, r2):
 
 
 def filter_response(response):
+    response = filter_response_headers(response)
+    response = filter_response_body_bearer(response)
+    return response
+
+
+def filter_response_headers(response):
     for header in FILTER_RESPONSE_HEADERS:
         # headers should be case insensitive, but for some reason they weren't for me
         response['headers'].pop(header.lower(), None)
         response['headers'].pop(header, None)
+    return response
+
+
+def filter_response_body_bearer(response):
+    if 'body' in response and 'string' in response['body']:
+        body_string = response['body']['string'].decode('utf8')
+        if 'Authorization:' in body_string and 'Bearer' in body_string:
+            body_string = re.sub(r'Bearer [0-9a-zA-Z._-]+', 'Bearer INVALID', body_string)
+        response['body']['string'] = body_string.encode('utf8')
     return response
 
 
