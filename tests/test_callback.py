6c5a3a8fcd39e7da9c8a3010c9740034cf630ef6
Evgeni Golov <evgeni@golov.de>
2026-04-10 10:25:22 +0200

    Filter out new callback entries in 2.21+
    ignore_errors, ignore_unreachable and changed_when_result are new in
    2.21, but we need to have our fixtures be compatible with older Ansible
    releases
    
    failed was made more consistent (so it now shows up in loops too), which
    is also not compatible with out tests

--- tests/test_callback.py
+++ tests/test_callback.py
@@ -42,7 +42,8 @@ def drop_incompatible_items(d):
     for k, v in d.items():
         if k in ['msg', 'start', 'end', 'delta', 'uuid', 'timeout', '_ansible_no_log', 'warn', 'connection',
                  'extended_allitems', 'loop_control', 'expand_argument_vars', 'retries', 'parent', 'parent_type', 'finalized', 'squashed', 'no_log',
-                 'listen', '_ansible_internal_redirect_list', 'exception', 'resolved_action', 'delay', '_resolved_action', 'is_handler']:
+                 'listen', '_ansible_internal_redirect_list', 'exception', 'resolved_action', 'delay', '_resolved_action', 'is_handler', 'changed_when_result',
+                 'ignore_errors', 'ignore_unreachable', 'failed']:
             continue
 
         if isinstance(v, dict):
@@ -76,6 +77,8 @@ def run_callback(tmpdir, report_type, vcrmode):
             contents = re.sub(r", \\\"expand_argument_vars\\\": true", "", contents)
             contents = re.sub(r", \\\"cmd\\\": null", "", contents)
             contents = re.sub(r", \\\"exception\\\": [^,]+", "", contents)
+            contents = re.sub(r", \\\"changed_when_result\\\": [^,]+", "", contents)
+            contents = re.sub(r", \\\"failed\\\": false", "", contents)
         real_contents = json.loads(contents)
         if report_type == "foreman":
             try:
